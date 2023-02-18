aa33bffe58e3f055d51c7b6dca3c3cf0aef06193
Dennis E. Hamilton <dennis.hamilton@acm.org>
2023-02-18 13:54:25 -0800

    0.0.5 VCrayVerCheck.c Covering error case

--- devKits/WinDev/VCrayApp/dev/cache/VCrayVerCheck.c
+++ devKits/WinDev/VCrayApp/dev/cache/VCrayVerCheck.c
@@ -1,4 +1,4 @@
-/* VCrayVerCheck.c 0.0.4            UTF-8                         2023-02-13
+/* VCrayVerCheck.c 0.0.5            UTF-8                         2023-02-18
    -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
                         DETERMINATION OF RAYLIB VERSION
@@ -11,9 +11,11 @@
    for and the value used if available.  Otherwise an unknown version is
    reported.
 
-   VCrayApp creates a small batch file that can be used by VCrayApp.bat to
-   determine what setup variations are required in the compilation options
-   and list of raylib files to compile into the cache.
+   VCrayApp creates a small batch file that can be used by VCrayApp.bat and
+   outer scripts to determine what setup variations are required in the
+   compilation options, the list of raylib files to cache, and the ability of
+   certain raylib features to be used and demonstrated, depending on the
+   version of Visual Studio Build Tools being used.
 
    VCrayVerCheck.c is compiled, with cache\ the current directory, using the
    command
@@ -52,24 +54,30 @@
 int main(void)
     {   /* Determining what, if any, RAYLIB_VERSION is defined */
 
+       if (EOF ==
        # if   !defined(RAYLIB_VERSION)
              fputs( "ECHO [VCrayApp] Using unknown version of raylib\n"
                     "SET VCRAYVER=unknown\n"
                     "EXIT 0\n",
-                    stdout);
+                    stdout)
        # else
               fputs( "ECHO [VCrayApp] Using version "
                            SHOW(RAYLIB_VERSION)
                            " of raylib\n"
                      "SET VCRAYVER=" SHOW(RAYLIB_VERSION) "\n"
                      "EXIT 0\n",
-                     stdout );
+                     stdout )
        # endif
 
-       return EXIT_SUCCESS;
+                )  // checking for unlikely fputs( ) failure case.
+            return EXIT_FAILURE;
+       else return EXIT_SUCCESS;
     }
 
-/* 0.0.4 2023-02-13T00:06Z Clean up reporting on version
+/*
+
+   0.0.5 2023-02-18T21:49 Add EOF handling of the fputs( ) used.
+   0.0.4 2023-02-13T00:06Z Clean up reporting on version
    0.0.3 2023-02-12T23:47Z Struggling to get TV(X) to glom the value
    0.0.2 2023-02-12T23:01Z Use VCshowDefs TV(X) definition to enstring the
          value of RAYLIB_VERSION
