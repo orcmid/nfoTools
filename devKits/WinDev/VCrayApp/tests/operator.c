e194d752a3c1f61321a211697a626cc895e6fe63
Dennis E. Hamilton <dennis.hamilton@acm.org>
2023-04-18 14:39:43 -0700

    0.0.1 VCrayApp tests\operator.c introduction #42 #55

---
+++ devKits/WinDev/VCrayApp/tests/operator.c
@@ -0,0 +1,36 @@
+/* operator.c 0.1.0              UTF-8                          2023-04-18
+   -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+
+   A quick check to demonstrate C++ operator+ is not available in C11.
+
+*/
+
+#include <raylib.h>
+
+Vector3 operator+(const Vector3& a, const Vector3& b)
+{
+    return (Vector3){a.x + b.x, a.y + b.y, a.z + b.z};
+    }
+
+int main()
+{
+    /* nothing to do here */
+
+    /* This VCrayApp test case is demonstrated standalone by copying
+       operator.c into dev\src\ and setting %VCAPPEXE% to operator.exe.
+        1. Run "VCrayApp.bat *" (terse).  Confirm :FAIL5.
+        2. If there are no visible diagnostics, use plain "VCrayApp.bat"
+           (verbose) * and capture the result.
+
+       On completion, delete operator.c from dev\src\.  Delete anything
+       that ended up in dev\app\, and restore %VCAPPEXE% to "ReplaceMe.exe"
+       */
+
+    return 0;}
+
+/* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+
+   0.1.0 2023-04-18T21:23Z VCrayApp tests version
+
+                        ***** end of operator.c *****
+   */
