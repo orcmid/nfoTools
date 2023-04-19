dbe470e8c2900376a6a8a36aa93b28dd6aa34232
Dennis E. Hamilton <dennis.hamilton@acm.org>
2023-04-19 09:31:27 -0700

    0.1.1 VCrayApp tests\operator.c touch-ups
    Describe using the test better.  Also get the same line 9 as reported in D211101 FAIL4 documentation

--- devKits/WinDev/VCrayApp/tests/operator.c
+++ devKits/WinDev/VCrayApp/tests/operator.c
@@ -1,9 +1,8 @@
-/* operator.c 0.1.0              UTF-8                          2023-04-18
+/* operator.c 0.1.1                 UTF-8                          2023-04-19
    -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
    A quick check to demonstrate C++ operator+ is not available in C11.
-
-*/
+   */
 
 #include <raylib.h>
 
@@ -16,20 +15,24 @@ int main()
 {
     /* nothing to do here */
 
+    return 0;
+    }
+
     /* This VCrayApp test case is demonstrated standalone by copying
        operator.c into dev\src\ and setting %VCAPPEXE% to operator.exe.
         1. Run "VCrayApp.bat *" (terse).  Confirm :FAIL5.
         2. If there are no visible diagnostics, use plain "VCrayApp.bat"
-           (verbose) * and capture the result.
+           (verbose, no "*") and capture the result.
 
        On completion, delete operator.c from dev\src\.  Delete anything
-       that ended up in dev\app\, and restore %VCAPPEXE% to "ReplaceMe.exe"
+       that ended up in dev\app\, and restore %VCAPPEXE% to "ReplaceMe.exe".
+       A clean way to restore VCrayApp.bat is using Git to restore the
+       working copy from its last check-in here in nfoTools.
        */
-
-    return 0;}
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+   0.1.1 2023-04-19T15:29Z Cleanup comments and layout
    0.1.0 2023-04-18T21:23Z VCrayApp tests version
 
                         ***** end of operator.c *****
