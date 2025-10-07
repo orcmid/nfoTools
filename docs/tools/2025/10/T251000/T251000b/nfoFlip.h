9cb6d091cb4ce09c210a1711f7b85baf39e935ae
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-07 16:40:18 -0700

    0.0.3 nfoFlip.h Addition of §s and describe nfoUniformRand( ) prototype

--- docs/tools/2025/10/T251000/T251000b/nfoFlip.h
+++ docs/tools/2025/10/T251000/T251000b/nfoFlip.h
@@ -1,4 +1,4 @@
-/* nfoFlip.h 0.0.2                  UTF-8                       2025-10-07
+/* nfoFlip.h 0.0.3                  UTF-8                       2025-10-07
 * --|----1----|----2----|----3----|----4----|----5----|----6----|----7----*
 *
 *              nfoFlip LAGGED-FIBONACCI PSEUDO-RANDOM NUMBER
@@ -11,9 +11,9 @@
 */
 
 
-void nfoInitRand(long seed);
+void nfoInitRand(long seed); // §11
 
-extern long * nfo_pFS;  // pointer into the nfoFlipState array
+extern long * nfo_pFS;  // pointer into the nfoFlipState array §6
 // XXX Explain how this pointer is always to the next random number in
 //     the array, the generated numbers are always non-negative, and
 //     the first element is always -1 to cause a new cycle to be
@@ -22,18 +22,19 @@ extern long * nfo_pFS;  // pointer into the nfoFlipState array
 
 #define nfoNextRand( ) (*nfo_pFS >= 0 ? *nfo_pFS-- : nfoFlipCycle( ))
 
-long nfoFlipCycle(void);
+long nfoFlipCycle(void); // §6
 // XXX To avoid failing the birthday spacing test, this function should
-// be called twice in nfoNextRand() or maybe the doubling can be in the
-// implementation of nfoFlipCycle().
+// be called twice in nfoNextRand().  See GB_FLIP §6.
 
 
-long nfoUniformRand(long max);
+long nfoUniformRand(long n); // §13
+// Generate a uniformly distributed random number in the range [0, n-1).
 
 
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+0.0.3 2025-10-07T23:35Z Add §s and describe nfoUniformRand( ).
 0.0.2 2025-10-07T23:06Z Touch-up
 0.0.1 2025-10-07-21:48Z Use nfo_pFS instead of nfo_fptr.
 0.0.0 2025-10-07-01:01Z Placeholder with draft starters.
