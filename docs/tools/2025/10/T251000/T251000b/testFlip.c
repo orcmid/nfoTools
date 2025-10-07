37623d48324ce409076a36627dbe71d6d4001e5e
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-07 14:55:33 -0700

    0.0.1 testFlip.c  Add § for citing locations in GB_FLIP description, touch-up

--- docs/tools/2025/10/T251000/T251000b/testFlip.c
+++ docs/tools/2025/10/T251000/T251000b/testFlip.c
@@ -1,4 +1,4 @@
-/* testFlip.c 0.0.0                 UTF-8                       2025-10-06
+/* testFlip.c 0.0.1                 UTF-8                       2025-10-07
 * --|----1----|----2----|----3----|----4----|----5----|----6----|----7----*
 *
 *                 nfoFlip IMPLEMENTATION CONFIRMATION TEST
@@ -8,7 +8,7 @@
 * known results.  More details are provided in nfoFlip.c.
 *
 * testFlip is a transposition of the test_flip.c program published by Donald
-* Knuth in the Literate Programming description of GB_FLIP, p. 216 in "The
+* Knuth in the Literate Programming description of GB_FLIP, p. 216 §2, in "The
 * Stanford GraphBase: A Platform for Combinatorial Computing".  See
 * https://www-cs-faculty.stanford.edu/~knuth/sgb.html.
 *
@@ -23,7 +23,7 @@ int main(void)
 {
     nfoInitRand(-314159L);
 
-    if (nfoNextRand () != 119318998)
+    if (nfoNextRand() != 119318998)
          { fputs(stderr, "testFlip: Failure on the first try!\n");
            exit(-1);
            }
@@ -43,7 +43,8 @@ int main(void)
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
-   0.0.0 2025-10-06-13:17Z Initial transposition based on Knuth's test_flip.c
+0.0.1 2025-10-07T21:31Z Added § for citing locations in GB_FLIP description.
+0.0.0 2025-10-06-13:17Z Initial transposition based on Knuth's test_flip.c
 
                         *** end of testFlip.c ***
 
