49c259e62696ff932a4ebfaf6f8a3b4dc56fddc6
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-08 21:00:52 -0700

    0.0.4 nfoFlip.c Introduce nfoInitRand, skeleton nfoUniformRand( )

--- docs/tools/2025/10/T251000/T251000b/nfoFlip.c
+++ docs/tools/2025/10/T251000/T251000b/nfoFlip.c
@@ -1,4 +1,4 @@
-/* nfoFlip.c 0.0.3                  UTF-8                       2025-10-08
+/* nfoFlip.c 0.0.4                  UTF-8                       2025-10-09
 * --|----1----|----2----|----3----|----4----|----5----|----6----|----7----*
 *
 *             nfoFlip LAGGED-FIBONACCI PSEUDO-RANDOM NUMBERS
@@ -32,8 +32,8 @@ extern long *_nfo_pFS = &FS[0];  // {§5 §6}
 
 long nfoFlipCycle(void)  // {§7}
 {  // Generate a new cycle of 55 pseudo-random numbers in the FS[] array
-   // using the lagged-Fibonacci method.  Return the first to be delivered in
-   // the new cycle, FS[55].
+   // using the lagged-Fibonacci method.  Return FS[55], the first of the
+   // new cycle.
 
    for (int i = 1; i <= 24; i++)
       FS[i] = mod_diff(FS[i + 31], FS[i]);
@@ -46,9 +46,59 @@ long nfoFlipCycle(void)  // {§7}
    return (FS[55]);          // Return the first of the new cycle.
    }
 
+void nfoFlipInit(long seed)  // {§8}
+{  // Initialize the FS[] array using the given seed.
+
+   long prev, next = 1;
+
+   seed = prev = mod_diff(seed, 0); // Strip off 2's complement sign bit
+
+   FS[55] = prev;
+   for (int i = 21; i; i = (i + 21) % 55)
+       { FS[i] = next;
+         /* Compute new next based on next, prev and seed §9 */
+         next = mod_diff(prev, next);
+         if (seed & 1)
+              seed = 0x40000000 + (seed >> 1); // rotating seed right 1
+         else seed = seed >> 1;
+         next = mod_diff(next, seed);
+         prev = FS[i];
+         }
+
+   /* Get the FS values "warmed up" §10 */
+   nfoFlipCycle( );
+   nfoFlipCycle( );
+   nfoFlipCycle( );
+   nfoFlipCycle( );
+   nfoFlipCycle( );  // ending with _nfo_pFS = &FS[54]
+
+   } /* nfoFlipInit */
+
+long nfoUniformRand(long n)  // {§12}
+{  // Generate a uniformly distributed random number in the range [0, n-1].
+
+/* XXX: REVIEW THIS CO_OP SUGGESTION AGAINST {§12}
+
+   long limit, x;
+
+   if (n <= 1)
+      return 0;  // trivial case
+
+   limit = (0x7fffffff / n) * n; // largest limit < 2**31 that is a
+                                  // multiple of n
+
+   do
+      x = nfoNextRand( );
+   while (x >= limit);
+
+   return (x % n);
+*/
+   }
+
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+0.0.4 2025-10-09T03:54Z Introduce nfoRandomInit(), skeleton nfUniformRand().
 0.0.3 2025-10-08T23:31Z Switch to _nfo_pfs and smooth nfoFlipCycle a bit.
 0.0.2 2025-10-07T22:45Z Add mod_diff and nfoFlipCycle aided by Co-Pilot.
 0.0.1 2025-10-07T21:52Z Use nfo_pFS instead of nfo_fptr. Use § section ids
