a88ed87748627f7ff187d161e4f5a90de3e458d2
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-14 08:59:25 -0700

    0.3.0 testflip.c Show versions in nfoTools-style console output

--- devLib/DEK/nfoFlip/dev/testFlip.c
+++ devLib/DEK/nfoFlip/dev/testFlip.c
@@ -1,4 +1,4 @@
-/* testFlip.c 0.2.0                 UTF-8                       2025-10-13
+/* testFlip.c 0.3.0                 UTF-8                       2025-10-14
 * --|----1----|----2----|----3----|----4----|----5----|----6----|----7----*
 *
 *                 nfoFlip IMPLEMENTATION CONFIRMATION TEST
@@ -8,9 +8,9 @@
 * known results.  More details are provided in nfoFlip.c.
 *
 * testFlip is a transposition of the test_flip.c program published by Donald
-* Knuth in the Literate Programming description of GB_FLIP, p. 216 §2, in "The
+* Knuth in the Literate Programming description of GB_FLIP, p. 216 §2 in "The
 * Stanford GraphBase: A Platform for Combinatorial Computing".  See
-* https://www-cs-faculty.stanford.edu/~knuth/sgb.html.
+* https://www-cs-faculty.stanford.edu/~knuth/sgb.html and nfoFlip.h here.
 *
 */
 
@@ -21,22 +21,24 @@
 
 int main(void)
 {
+    fputs("\n[testFlip] 0.3.0 Confirmation of " nfoFlipVersion "\n", stdout);
+
     nfoFlipInit(-314159L);
 
     long first = nfoFlipNextRand();
     if (first != 119318998)
-         { fputs("testFlip: Failure on the first try!\n", stderr);
+         { fputs("           FAILURE ON THE FIRST TRY!\n\n", stdout);
            exit(-1);
            }
 
     for (int j = 1; j <= 133; j++) nfoFlipNextRand();
 
     if (nfoFlipUniformRand(0x55555555L) != 748103812)
-         { fputs("testFlip: Failure on the second try!\n", stderr);
+         { fputs("           FAILURE ON THE SECOND TRY!\n\n", stdout);
            exit(-2);
            }
 
-    fputs("testFlip: OK, the nfoFlip routines seem to work.\n", stdout);
+    fputs("           OK, the nfoFlip routines seem to work.\n\n", stdout);
 
     exit(EXIT_SUCCESS);
 
@@ -44,6 +46,7 @@ int main(void)
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+0.3.0 2025-10-14T15:56Z Update to Report nfoFlip version in nfoTools style
 0.2.0 2025-10-13T22:15Z Adjust to current nfoFlip function names
 0.1.1 2025-10-10T01:40Z Write fputs operands in correct order
 0.1.0 2025-10-09T23:48Z Rename to the adjusted nfoFlip.h/c names
