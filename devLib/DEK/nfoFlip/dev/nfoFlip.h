188db4fa993d8635adb39b3c073c060fbe73137b
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-14 08:23:26 -0700

    0.1.2 nfoFlip.h Adding nfoFlipVersion and smooth some comments

--- devLib/DEK/nfoFlip/dev/nfoFlip.h
+++ devLib/DEK/nfoFlip/dev/nfoFlip.h
@@ -1,39 +1,41 @@
-/* nfoFlip.h 0.1.1                  UTF-8                       2025-10-13
+/* nfoFlip.h 0.1.2                  UTF-8                       2025-10-14
 * --|----1----|----2----|----3----|----4----|----5----|----6----|----7----*
 *
 *              nfoFlip LAGGED-FIBONACCI PSEUDO-RANDOM NUMBERS
 *              ----------------------------------------------
 *
-* nfoFlip is a transposition of the Literate Program GB_FLIP presented by
-* Donald Knuth in pp. 216-221 of "The Stanford GraphBase: A Platform for
-* Combinatorial Computing", Addison-Wesley (Reading MA, 1993).  See
-* <https://www-cs-faculty.stanford.edu/~knuth/sgb.html> where the original
-* CWEB code is available.  <https://github.com/ascherer/sgb> is a GitHub
-* repository of that source provided and maintained by Andreas Scherer.
+* nfoFlip is a transposition of Donald Knuth's Literate Program GB_FLIP
+* in pp. 216-221 of "The Stanford GraphBase: A Platform for Combinatorial
+* Computing", Addison-Wesley (Reading MA, 1993).  For the original CWEB code,
+* see <https://www-cs-faculty.stanford.edu/~knuth/sgb.html>. Also see the
+* GitHub repository  <https://github.com/ascherer/sgb> for the SGB files
+* provided and maintained by Andreas Scherer.
 *
 * The §n section numbers refer to sections in the GB_FLIP text from which
 * nfoFlip is derived.
 *
 * WARNING: nfoFlip is not thread-safe.  A single instance cannot be safely
-*    used by multiple threads in the same application.
+*    used across multiple threads in the same program.
 *
 * STABILITY: nfoFlip procedures nfoFlipInit, nfoFlipNextRand, and
 *     nfoFlipUniformRand provide the same functions as the GB_FLIP procedures
 *     gb_init_rand, gb_next_rand, and gb_uniform_rand, respectively.  This
 *     means that
-*      - nfoFlip is a drop-in alternative for GB_FLIP in other programs
+*      - nfoFlip is an alternative for GB_FLIP in adaptation of other programs
 *      - the same sequences of nfoFlipInit, nfoFlipNextRand, and
 *        nfoFlipUniformRand with the same seed will yield an identical
 *        sequence of results.
 */
 
+#define nfoFlipVersion "nfoFlip-1.0.0" // initial release version
+
 void nfoFlipInit(long seed); // §11
 /* TODO: Recommend seed choices, and using the same seed for reproducibility
    */
 
-extern long * _nfo_pFS;  // §5 pointer into the nfoFlip state array.
-/* _nfo_pFS is managed privately by nfoFlip.c in support of the high-
-   performance macro nfoFlipNextRand() defined below. It must not be used directly in user code.
+extern long * _nfo_pFS;  // §5 pointer maintained privately by nfoFlip.c
+/* _nfo_pFS is used in the operation of nfoFlipNextRand() defined below.
+   It is not intended for use in user code.
    */
 
 #define nfoFlipNextRand( ) (*_nfo_pFS >= 0 ? *_nfo_pFS-- : nfoFlipCycle( ))
@@ -55,6 +57,7 @@ long nfoFlipUniformRand(long n); // §13
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+0.1.2 2025-10-14T15:21Z Add nfoFlipVersion, smooth some comments.
 0.1.1 2025-10-13T22:01Z Use consistent nfoFlipForm naming, smooth comments.
 0.1.0 2025-10-09T23:41Z Rename to consistent nfoFlipInit( ) and nfoNextFlip( )
 0.0.4 2025-10-08T23:32Z Switch to _nfo_pFS and rework comments.
