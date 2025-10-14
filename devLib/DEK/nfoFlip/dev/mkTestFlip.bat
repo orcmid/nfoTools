aaa4b51009a22da016b8a961176b84aadf8e2b27
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-14 07:50:56 -0700

    0.0.1 mkTestFlip.bat with @VCflip.opt for maximum optimization

--- devLib/DEK/nfoFlip/dev/mkTestFlip.bat
+++ devLib/DEK/nfoFlip/dev/mkTestFlip.bat
@@ -1,5 +1,5 @@
 @echo off
-REM mkTestFlip.bat 0.0.1            UTF-8                         2025-10-14
+REM mkTestFlip.bat 0.0.0            UTF-8                         2025-10-13
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
 rem                          COMPILING testFlip
@@ -10,6 +10,7 @@ CL /Fe:testFlip.exe testFlip.c nfoFlip.c @VCflip.opt
 
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
-rem 0.0.1  2025-10-14T01:35Z Initial version.
+rem 0.0.1  2025-10-14T14:48Z Add @VCflip.opt for optimization options.
+rem 0.0.0  2023-06-14T01:35Z Initial version.
 rem
 rem                     *** end of mkTestFlip.bat ***
