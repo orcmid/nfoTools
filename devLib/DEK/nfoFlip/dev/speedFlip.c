d12fd860be53fcd34d255cb0f98ee0d0d0bc38bc
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-10-16 16:03:00 -0700

    0.1.1 speedFlip.c displaying commas in repetition reporting, small touch-ups

--- devLib/DEK/nfoFlip/dev/speedFlip.c
+++ devLib/DEK/nfoFlip/dev/speedFlip.c
@@ -1,4 +1,4 @@
-/* speedFlip.c 0.0.7                 UTF-8                       2025-10-15
+/* speedFlip.c 0.1.1                 UTF-8                       2025-10-16
 * --|----1----|----2----|----3----|----4----|----5----|----6----|----7----*
 *
 *                          nfoFlip SPEED TESTING
@@ -14,6 +14,23 @@
 
 #include "nfoFlip.h"
 
+char * const commaNumbered(unsigned long long n)
+{   /* Express the number with commas (Co-Pilot assisted) */
+
+    static char commad[32];
+    int p = sizeof(commad) - 1;
+
+    commad[p] = '\0';
+
+    do { if (p % 4 == 0 && p != sizeof(commad) - 1)
+              commad[--p] = ',';
+         commad[--p] = '0' + (n % 10);
+         n /= 10;
+         } while (n != 0);
+
+    return &commad[p];
+    } /* commaNumbered */
+
 int main(int argc, char *argv[])
 {
     unsigned long long const defaultReps = 10000000000;
@@ -24,12 +41,14 @@ int main(int argc, char *argv[])
 
     long lastRand;                 /* final random number obtained */
 
-    printf("\n[speedFlip] 0.0.0 Performance checking %s\n",
+    printf("\n[speedFlip] 0.1.1 Performance checking %s\n",
            nfoFlipVersion);
 
 
     if (argc > 1) reps = strtoull(argv[1], NULL, 10);
     if (reps == 0) reps = defaultReps;
+
+    char * commad = commaNumbered(reps);
 
     nfoFlipInit(-314159L);
 
@@ -39,9 +58,9 @@ int main(int argc, char *argv[])
     finishTime = time(NULL);
     elapsed = difftime(finishTime, startTime);
 
-    printf( "\n            %llu nfoFlipNextRand()s took %.0f seconds",
-            reps, elapsed);
-    printf( "\n            ending with  %ld\n", lastRand);
+    printf( "\n            %s nfoFlipNextRand()s took %.0f seconds",
+            commad, elapsed);
+    printf( "\n            ending with %ld\n", lastRand);
 
     startTime = time(NULL);
     for (unsigned long long i = 0; i < reps; i++)
@@ -49,22 +68,22 @@ int main(int argc, char *argv[])
     finishTime = time(NULL);
     elapsed = difftime(finishTime, startTime);
 
-    printf( "\n            %llu nfoFlipUniformRand()s more took %.0f seconds",
-            reps, elapsed);
-    printf( "\n            ending with  %ld\n\n", lastRand);
-
+    printf( "\n            %s nfoFlipUniformRand()s more took %.0f seconds",
+            commad, elapsed);
+    printf( "\n            ending with %ld\n\n", lastRand);
 
     exit(EXIT_SUCCESS);
 
     } /* main */
 
-/* TODO: Find a better way to enter durations as powers of ten.*/
 /* TODO: Have a Help option */
 
-/* TODO: Report repetitions in comma-form numbers */
+/* TODO: Allow commas in the input parameter */
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+0.1.1 2025-10-16T22:56Z cleanup spacing in output lines, update version
+0.1.0 2025-10-16T22:47Z Change to commaNumbered output of reps
 0.0.7 2025-10-15T22:59Z Omit decimal places, add TODO
 0.0.6 2025-10-15T22:43Z Use external nfoFlipVersion
 0.0.5 2025-10-15T05:17Z Add TODOs
