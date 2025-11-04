e4cb9d3a2658a51e99914753aeee52337ee105e4
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-11-04 08:37:57 -0800

    0.0.4 nfoGenRandsKit/dev/combo.h Cleanup explanations, Marsaglia attribution

--- devLib/DieHard/nfoGenRandsKit/dev/combo.h
+++ devLib/DieHard/nfoGenRandsKit/dev/combo.h
@@ -1,40 +1,40 @@
-/* combo.h 0.0.3                    UTF-8                         2025-10-30
+/* combo.h 0.0.4                    UTF-8                         2025-11-04
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 *
-*        COMBO RANDOM NUMBER GENERATOR NFOGENRANDS CUSTOMIZATION
-*        -------------------------------------------------------
+*                 MARSAGLIA'S COMBO RANDOM NUMBER GENERATOR
+*                 -----------------------------------------
 *
-*   combo.h is a customization of nfoGenRands.h. It is based on George
-*   Marsaglia's COMBO RNG included in the original DIEHARD software.
+*   combo.h is a header-only derivative of George Marsaglia's COMBO
+*   RNG included in the original DIEHARD software.
+*
+*   It works by combining two separate RNGs such that the combined period
+*   exceeds 2^60.5.  These are Marsaglia's claims and description.
 
-*   period> 2^60.5
-*   x(n)=x(n-1)*x(n-2) mod 2^32 period is 3*2^29 if seeds odd and one is +or-3
-*   mod
-*   period of x(n)=x(n-1)*x(n-2) is 3*2^29 if seeds odd, and one is +or-3 mod
-*   easy to ensure: replace seed x with 3*x*x.
-*   mwc z=30903*iand(z,65535)+ishft(z,-16)
+*   The first RNG is x(n)=x(n-1)*x(n-2) mod 2^32 with a period of 3*2^29
+*   when the seeds are odd and one is 3 or 5 mod 8.  This is assured in
+*   the seeding.
 *
-*   FIND BETTER EXPLANATION, USING THIS FROM makef.txt FROM diehard-fortran.
-::   This program creates the binary file combo.32, containing   ::
-     :: 11+ megabytes of integers from a simple but very good combi-  ::
-     :: nation generator.  It combines, by addition mod 2^32,         ::
-     ::             x(n)=x(n-1)*x(n-2) mod 2^32                       ::
-     ::    and                                                        ::
-     ::             y(n)=30903*y(n-1) + carry mod 2^16                ::
-     :: The period of the first is 3*2^29, on odd integers, and the   ::
-     :: period of the second, a multiply-with-carry generator, is     ::
-     :: 30903*2^15-1=1012629503, so the period of combo exceeds 2^60. ::
-     :: This generator is simple to program in Fortran or C and quite ::
-     :: fast.   It seems to pass all tests in DIEHARD.  Try it.       ::
-     :: You will be prompted for three seed integers.  The x's of the ::
-     :: seeds x1,x2,y must be 3 or 5 mod 8, which is ensured by repla-::
-     :: cing  x1 by 3*(x1+x1+1)^2 and x2 by 2*x2+1.
+*   The second RNG is a multiply-with-carry (MWC) generator
+*   z(n) = 30903*z(n-1) + carry mod 2^16 where carry = floor(z(n-1)/2^16).
+*   That period is 30903*2^15-1=1012629503
+*
+*   In th original Fortran code, the MWC is implemented as
+*   z(n) = 30903*iand(z(n-1),65535)+ishft(z(n-1),-16)
+*
+*   The two results are combined by addition: x(n)+z(n)mod 32.
+*
+*   This generator seems to pass all tests in DIEHARD and is quite fast.
+*   It is simple to program in Fortran or C.
+*
+*   The present version is offered in nfoTools devLib as useful and also
+*   for verification of DieHard operation and results.
+*
 */
 
-#define NFOGENVERSION "combo-0.0.2"
+#define NFOGENVERSION "combo-0.0.4"
 
-    long Xcombo, Ycombo, Zcombo;
-        /* Wonky names for x, y, z to avoid possible name clashes
+    unsigned long Xcombo, Ycombo, Zcombo;
+        /* Wonky names for original x, y, z to avoid possible name clashes
            where combo.h might be included.  Theses are going to be
            static variables in the program that includes combo.h
            */
@@ -46,26 +46,28 @@
           Zcombo = z;
           }
 
-    long combo_rand()
-        { long v = Xcombo * Ycombo;
-          Xcombo = Ycombo;
-          Ycombo = v;
+    unsigned long combo_rand()
+        { unsigned long v = Xcombo * Ycombo;   // new x(n)
+          Xcombo = Ycombo;                     // next x(n-2)
+          Ycombo = v;                          // next x(n-1)
           Zcombo = 30903 * (Zcombo & 65535) + (Zcombo >> 16);
             /* TODO: Ensure Zcombo >> 16 matches the Fortran right-shift.
                In the Fortran ishift(), 0 is shifted in on the left,
                but in Standard C that is assured only for unsigned types. */
-          return Ycombo + Zcombo;
+          return v + Zcombo;
           }
 
 /* TODO Plenty
    * Confirm that the range is 0 to 2^32-1.
-   * Add a version number that can be used in nfoGenRands.c identification.
    * I need a way to test this with what makewhat.f was going to produce.
      Perhaps this can be found in the C Language version.
+   * We are operating on the assumption that unsigned long is exactly 33 bits.
+   * It might be better to use uint32_t from stdint.h.
    */
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+   0.0.4  2025-11-04T16:27Z Create free-standing and testable combo.h
    0.0.3  2025-10-30T20:22Z Hand wring about range and unsigned values
    0.0.2  2025-10-26T16:55Z Keep noodling
    0.0.1  2025-10-25T21:52Z Paste in description from diehard makef.txt.
