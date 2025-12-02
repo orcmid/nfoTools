bc212427872a0371f4540ac0d87db8d36d9f0713
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-12-02 15:05:05 -0800

    0.0.1 nfoGenAIO-test1.c working setup and mkGenAIO-test1.bat compiling

---
+++ devLib/DieHard/nfoGenRandsKit/tests/mkGenAIO-test1.bat
@@ -0,0 +1,15 @@
+@echo off
+REM mkGenAIO-test1.bat 0.0.0        UTF-8                         2025-12-02
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem                   COMPILING nfoGenAIO TEST PROGRAM #1
+rem
+rem    Compile nfoGenAIO-test1.exe with optionization for maximum speed.
+
+CL @VCoptions.opt nfoGenAIO-test1.c ../dev/nfoGenAIO.c
+
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem 0.0.0  2025-12-02T22:54Z Initial version.
+rem
+rem                  *** end of mkGenAIO-test1.bat ***
---
+++ devLib/DieHard/nfoGenRandsKit/tests/nfoGenAIO-test1.c
@@ -0,0 +1,35 @@
+/* nfoGenAIO-test1.c 0.0.0          UTF-8                         2025-12-02
+/* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+*
+*                    TEST 1: nfoGenAIO ASCII OUTPUT
+*                    ------------------------------
+*
+*   This test does some simple output cases to verify the correct results
+*   for some simple word patterns in 8-character hexadecimal. It should be
+*   easy to sightcheck the output.  When successful, this test is also a
+*   nice one to pipe into a read test input.
+*
+*   The test output is written to stdout, which may be redirected or piped
+*   as desired for further processing.
+*/
+
+#include  <stdio.h>
+#include  <stdint.h>
+#include  <stdlib.h>
+
+#include  "nfoGenAIO.h"
+
+int main(void)
+{
+
+
+  return EXIT_SUCCESS;
+}
+
+/* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+*
+* 0.0.1  2025-12-02T23:01Z After simple fix here, perfectly clean compile !!
+* 0.0.0  2025-12-02T22:58Z Stub version to get minimal compile error checking.
+*
+*                  *** end of nfoGenAIO-test1.c ***
+*/
