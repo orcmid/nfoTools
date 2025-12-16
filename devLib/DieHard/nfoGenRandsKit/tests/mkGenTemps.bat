e4f6c7eaaf5f568dfbf0aef05facfcb8cb6ad049
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-12-16 13:00:46 -0800

    0.0.1 nfoGenBIO-Win32-temps and mkGenTemps script

---
+++ devLib/DieHard/nfoGenRandsKit/tests/mkGenTemps.bat
@@ -0,0 +1,15 @@
+@echo off
+REM mkGenTemps.bat 1.0.0            UTF-8                         2025-12-16
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem                   COMPILING nfoGenAIO TEST PROGRAM #2
+rem
+rem    Compile nfoGenAIO-test1.exe with optionization for maximum speed.
+
+CL @VCoptions.opt /FeGenTemps.exe nfoGenBIO-win32-temps.c
+
+rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+rem
+rem 1.0.0  2025-12-16T20:59Z Initial version.
+rem
+rem                    *** end of mkGenTemps.bat ***
--- devLib/DieHard/nfoGenRandsKit/tests/nfoGenBIO-win32-temps.c
+++ devLib/DieHard/nfoGenRandsKit/tests/nfoGenBIO-win32-temps.c
@@ -1,4 +1,4 @@
-/* nfoGenBIO-Win32-temps.c 0.0.0    UTF-8                         2025-12-16
+/* nfoGenBIO-Win32-temps.c 0.0.1    UTF-8                         2025-12-16
 ** -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 *
 *                   nfoGenBIO Temporary File Experiments
@@ -17,27 +17,62 @@
 *   The Microsoft Learn documentation is not very helpful and the C11
 *   documentation that I have may not be what has become standard at some
 *   level of the ISO Standard for the C Language.
+*
+*   NOTE: This exercise is specific to the Windows 10 configuration at
+*         orcmid's personal computer.  Other Windows configurations may
+*         be suitable; that is not being considered here.
+*
 *   -----------------------------------------------------------------------
-*   Attribution: This program is copilot assisted.
+*   Attribution: This program is co-pilot assisted.
 *   -----------------------------------------------------------------------
 */
 
+#include <io.h>
+    /* for Microsoft-specific _mktemp_s() */
+
 #include <stdint.h>
+
 #include <stdio.h>
 
-#include <io.h>
+#include <stdlib.h>
 
-/* XXX: Provisionally: Use _mktemp_s generated temporary filename using a
-        given basename template "baseXXXXXX", where base is chosen to reflect
-        the application (such as the RNG being tested).  The XXXXXX will be
-        changed in-place to a unique alphanumeric string by _mktemp_s( ).
 
-        The result is a rewrite filename without any path components.
 
-        Examples tend to use 8-character names with no file extension.  That
-        should be acceptable here, but I wonder if it can be done better.
+int main( void )
+{   /* The first test is to generate two temp names, confirming how the
+       successive uses of a "template" work.
 
-        Following the last read and closing of an input file, a remove
+       Preference for _mktemp_s is that it generates just the file name
+       and not a full path and all of that gunk. */
+
+       fputs ( "[GenTemps] Temporary Filename Generation Experiment 0.0.1\n",
+            stdout );
+
+    char tmpname[ ] = "nfoGenBIOXXXXXX";
+
+    errno_t err = _mktemp_s( tmpname, sizeof( tmpname ) );
+
+
+    if ( err != 0 )
+         {  printf( "           _mktemp_s ERRNO: %d\n", err );
+            return EXIT_FAILURE;
+            }
+
+    printf( "           First temporary filename: %s\n", tmpname );
+
+    errno_t err = _mktemp_s( tmpname, sizeof( tmpname ) );
+
+    if ( err != 0 )
+         {  printf( "           Second _mktemp_s ERRNO: %d\n", err );
+            return EXIT_FAILURE;
+            }
+
+    printf( "           Second temporary filename: %s\n", tmpname );
+
+    return EXIT_SUCCESS;
+    } /* GenTemps */
+
+/* XXX: Following the last read and closing of an input file, a remove
         operation might be performed.  This should be the case if the name
         is obtained via piping.  This could be confusing.  Think this through.
         Maybe need L_tmpnam_s and tmpnam_s( ).
@@ -62,11 +97,6 @@
         and now to get control of it, especially with regard to choosing
         the file locations in some simple way.
 
-        Use of <io.h> is required for _mktemp_s( ).  I would like to use
-        Standard C <stdio.h> functions, but they don't seem to provide the
-        required flexibility on Windows.  I will try them and see what
-        happens, especially with regard to file locations and path names.
-
         Since I may need to use Windows-specific <direct.h> functions, and
         maybe have to look at settings, I may already be in a state of sin.
         */
@@ -76,7 +106,7 @@
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
-
+   0.0.1  2025-12-16T20:49Z Initial experiment with _mktemp_s.
    0.0.0  2025-12-16T18:09Z Initial placeholder.
 
                     *** end of nfoGenBIO-Win32-temps.c ***
