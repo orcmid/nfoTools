9859e5cbf4986538a2a35f4d0f80b7c91badcc87
Dennis E. Hamilton <dennis.hamilton@acm.org>
2025-12-19 14:24:05 -0800

    0.0.4 nfoGenBIO.h Cleanup, addition of StartOutput prototype and description

--- devLib/DieHard/nfoGenRandsKit/dev/nfoGenBIO.h
+++ devLib/DieHard/nfoGenRandsKit/dev/nfoGenBIO.h
@@ -1,4 +1,4 @@
-/* nfoGenBIO.h 0.0.3                UTF-8                         2025-12-16
+/* nfoGenBIO.h 0.0.4                UTF-8                         2025-12-19
 ** -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 *
 *                 nfoGenBIO Binary Input/Output Data Files
@@ -16,7 +16,7 @@
 *   consumed in blocks of 4096 32-bit words.
 *
 *   IMPORTANT: nfoGenBIO-formatted files are intended for generated data
-*      retained only long enough for processing in DieHard (and other) tests.
+*      retained only long enough for processing in DieHard and similar tests.
 *      The files need not be retained further, and they are not suitable for
 *      interchange.  When used as streams of random-number generator output,
 *      they are theoretically not compressible for storage (something that
@@ -32,31 +32,54 @@
 *         it can be useful to pipe the names of the files.
 */
 
-#define NFOGENBIO_VERSION "nfoGenBIO-0.0.2"
+#define NFOGENBIO_VERSION "nfoGenBIO-0.0.4"
    /* Version string for this nfoGenBIO module.  It is coordinated with the
     * version in nfoGenBIO-*.c for the platform-specific components.
-      */
+    */
 
 #include <stdint.h>
 
 #include <stdio.h>
 
 
+FILE* nfoGenBIO_startOutput( char *template);
+    /* Generate a temporary file name based on template, open it for binary
+     * writing, and return the FILE* for the opened file. The template string
+     * is replaced with the created filename.
+     *
+     * If template is NULL or the string is in incorrect format, NULL is
+     * returned.
+     *
+     * If the generated filename cannot be opened for writing, NULL is also
+     * returned. In this case, template has been modified.
+
+     * Otherwise, a non-NULL FILE* is returned for use in nfoGenBIO_write()
+     * operations.  The file can also be rewound and read.  Perform fclose()
+     * when all writing and any reading, are complete.
+     *
+     * The modified template can also be communicated for use in a subsequent
+     * file-open operation in the same or different application.
+     *
+     * TODO: Explain template format and usage for use in conjunction with
+     *       nfoGenRands and DieHard tests.
+     */
+
+
 size_t nfoGenBIO_write(uint32_t *buf, size_t nwords, FILE *fp)
-  { /* writes nwords values from buf[0] to buf[nwords-1] out to fp in direct
-    *  binary format.
-    *
-    * The file *fp must be opened for binary writing before calling this
-    * function.
-    *
-    * The return value is the number of words actually written.  It may be
-    * less than nwords if an error occurs during writing or if the the space
-    * available for the file is exhausted.
-    *
-    * Standard outputs (stdout and stderr) must not be used.
-    *
-    * Perform fclose(fp) when all writing is complete.
-    */
+  { /* writes nwords of values from buf[0] to buf[nwords-1] to fp in direct
+     *  binary format.
+     *
+     * The file *fp must be opened for binary writing before calling this
+     * function.
+     *
+     * The return value is the number of words actually written.  It may be
+     * less than nwords if an error occurs during writing or if the the space
+     * available for the file is exhausted.
+     *
+     * Standard outputs (stdout and stderr) must not be used.
+     *
+     * Perform fclose(fp) when all writing is complete.
+     */
 
     if (buf == NULL || fp == NULL || nwords == 0)
          return 0;
@@ -94,6 +117,8 @@ size_t nfoGenBIO_read(uint32_t *buf, size_t nwords, FILE *fp);
 
 /* -|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
+0.0.4  2025-12-19T22:22Z Cleanup, add SartOutput prototype and description
+0.0.3  2025-12-16T05:30Z (skipped, used in error at top of 0.0.2)
 0.0.2  2025-12-16T05:30Z More pondering, touching up comments.
 0.0.1  2025-12-12T20:35Z Complete read and write procedures
 0.0.0  2025-12-12T01:27Z Initial draft.
