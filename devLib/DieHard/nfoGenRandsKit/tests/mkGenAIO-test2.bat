8aa01b34a4b547bda2c3a5bd69d779a0377d7e8f
Dennis E. Hamilton <dennis.hamilton@acm.org>
2026-01-02 13:59:48 -0800

    2.0.0 mkGenAIO-test2.bat Include nfoGenAIO-win32.c, correct remark

--- devLib/DieHard/nfoGenRandsKit/tests/mkGenAIO-test2.bat
+++ devLib/DieHard/nfoGenRandsKit/tests/mkGenAIO-test2.bat
@@ -1,15 +1,16 @@
 @echo off
-REM mkGenAIO-test2.bat 1.0.0        UTF-8                         2025-12-04
+REM mkGenAIO-test2.bat 2.0.0        UTF-8                         2026-01-02
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
 rem                   COMPILING nfoGenAIO TEST PROGRAM #2
 rem
-rem    Compile nfoGenAIO-test1.exe with optionization for maximum speed.
+rem    Compile nfoGenAIO-test2.exe with optionization for maximum speed.
 
-CL @VCoptions.opt nfoGenAIO-test2.c ../dev/nfoGenAIO.c
+CL @VCoptions.opt nfoGenAIO-test2.c ../dev/nfoGenAIO.c ../dev/nfoGenAIO-win32.c
 
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
+rem 2.0.0  2026-01-02T21:51Z Update to include nfoGenAIO-win32.c for Windows.
 rem 1.0.0  2025-12-04T00:28Z Initial version.
 rem
 rem                  *** end of mkGenAIO-test2.bat ***
